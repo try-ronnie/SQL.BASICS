7ba61c3d9bb9c9115358d43e2ca3a3fa14b30f34
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-04 23:18:52 +0300

    finishing organizing the bears lab finally with the correct sql format hopefully

--- python-p3-organizing-bears-lab/lib/sql_queries.py
+++ python-p3-organizing-bears-lab/lib/sql_queries.py
@@ -10,25 +10,29 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
     SELECT
         bears.name
     FROM bears
-    Order by name
+    Order by name;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT 
-        bears.name
+        bears.name,
         bears.age
     FROM bears
     WHERE alive = 1 
-    ORDER BY age
+    ORDER BY age;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
     SELECT
-        bears.name
+        bears.name,
         bears.age
     FROM bears
-    WHERE age 
+    WHERE age = (SELECT MAX(age) FROM bears);
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name
+        bears.age
+    FROM bears 
+    WHERE age = (SELECT MIN(age)FROM bears)
 """
