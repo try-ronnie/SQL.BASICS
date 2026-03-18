ce9462e6764166d7f3e82039afccadba8de63a0f
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-18 19:58:46 +0300

    adding LOGIC THAT I THINK MATTERS

--- LEARNING.SQL.OPERATIONS/example 4/CREATE students.table.sql
+++ LEARNING.SQL.OPERATIONS/example 4/CREATE students.table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE students(
-id INTERGER PRIMARY KEY , 
-name TEXt NOT NULL, 
+id INTEGER PRIMARY KEY , 
+name TEXT NOT NULL, 
 age INTEGER NOT NULL CHECK (age > 10 AND age < 18),
 gender TEXT NOT NULL CHECK (gender in ('M', 'F'))
 );
