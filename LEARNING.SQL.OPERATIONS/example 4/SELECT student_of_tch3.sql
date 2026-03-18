e0a6eecf11b7730f03a6bec2d90413c34207cc0a
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-18 20:25:46 +0300

    adding the first join tables as in exaple 4

---
+++ LEARNING.SQL.OPERATIONS/example 4/SELECT student_of_tch3.sql
@@ -0,0 +1,8 @@
+SELECT students.name, students.id
+FROM students
+INNER JOIN students_teachers
+ON students.id = students_teachers.student_id
+WHERE students_teachers.teacher_id = 1
+;
+/*
+this is an inner join
