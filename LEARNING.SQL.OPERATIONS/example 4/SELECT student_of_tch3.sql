0af6ef822587fef9f6ed6b0b2de231f7a2363afc
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-19 01:40:35 +0300

    nikumwecheche

--- LEARNING.SQL.OPERATIONS/example 4/SELECT student_of_tch3.sql
+++ LEARNING.SQL.OPERATIONS/example 4/SELECT student_of_tch3.sql
@@ -5,4 +5,6 @@ ON students.id = students_teachers.student_id
 WHERE students_teachers.teacher_id = 1
 ;
 /*
-this is an inner join
+this is an inner join 
+its returns all the data from the left table  and the right table only if they have a mtaching relationships
+*/
