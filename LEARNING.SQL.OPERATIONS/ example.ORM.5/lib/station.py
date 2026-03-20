992293c85afc418d876b935301d103e09915eeff
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-20 13:47:20 +0300

    dding onto info or commiting cause we want those greens

--- LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/station.py
+++ LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/station.py
@@ -2,5 +2,11 @@ from __init__ import CURSOR , CONN
 
 
 class Station:
-    def __init__ (self,name , capacity , location , id):
-        self
+    def __init__ (self,name: str, capacity: int , location: str , id = None):
+        self.name = name
+        self.capacity = capacity
+        self.location = location
+    #creation of the table
+    def create_table(cls):
+        '''CREATING the table structure using the cls method'''
+        
