3fece946cd150ba4c238439faea1d18b6eeed5a7
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-24 11:33:20 +0300

    wass good

--- LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/provider.py
+++ LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/provider.py
@@ -1,3 +1,6 @@
 # creating a class provider ands its table and ways to relate data from the table and back to the class
-
-class Provider 
+from __init__ import CONN , CURSOR
+class Provider :
+    #initialize the instance
+    def __init__(self):
+         
