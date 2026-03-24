7620b486d6b1048be3f2b967a1ae96792cd2d968
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-03-24 11:32:07 +0300

    after a long break ;]

--- LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/__init__.py
+++ LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/__init__.py
@@ -1,9 +1,6 @@
+#initializing the database connection using sqlite
 import sqlite3
 
-CONN = sqlite3.connect('station.db')
+CONN = sqlite3.connect('oil_reolution')
 CURSOR = CONN.cursor()
 
-# to allow the initializing or connecting of a class with the database sqlite3 gives use methods to carry it out
-
-#sqlite3 .connect .... - this is the function that ensures connection to the databas
-#CURSOR => this is th method that allows execution of sql queries in our current connection to the database 
---
+++ LEARNING.SQL.OPERATIONS/ example.ORM.5/lib/provider.py
@@ -0,0 +1,3 @@
+# creating a class provider ands its table and ways to relate data from the table and back to the class
+
+class Provider 
